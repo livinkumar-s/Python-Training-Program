06d6d729895365e0b56306e70b88f8989d755a6a
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-18 12:07:59 +0530

    Intro --> Variables, Datratypes, Type casting

---
+++ Intro.py
@@ -0,0 +1,35 @@
+# print(123)
+# print(456)
+
+# the below line of code is wrong
+# Print(34)
+
+'''hgerhaeryh
+earhyszeryhseryhse
+dzryhseyserysrt'''
+
+# client_id=657647564654
+# print(client_id)
+
+# a=10
+# a=9
+# print(a)
+
+# a=10
+# b=1.0
+# str1="Hello world"
+# str2="Hi world"
+
+# print(type(str1)) #<class 'str'>
+
+# print(a+b)
+# print(str1+str2)
+# print(str1+2)
+
+name="Yash"
+age=33 #"33"
+
+# His name is Yash. He is 33 years old...!
+
+print("His name is "+name +". His age is "+str(age)+" years old...!")
+ 
