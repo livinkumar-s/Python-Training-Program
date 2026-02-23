52cc7a691e941ad9d09c266652f1875f88a5393b
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-23 12:05:20 +0530

    Control Flow --> Conditional Clause

---
+++ ControlFlow.py
@@ -0,0 +1,101 @@
+# print("Step1")
+# print("Step2")
+# print("Step3")
+# print("Step4")
+# print("Step5")
+
+# print(12)
+
+# a=int(input("Enter your mark: ")) #"40"
+# print(a+40)
+
+# age=int(input("Enter Your Age: "))
+# if age>=18:
+#     print("You can vote...!")
+# else:
+#     print("You cannot vote...!")
+
+# a=10
+
+# if a<10:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("Step4")
+#     print("Step5")
+# else:
+#     print("From else block..!")
+
+# print("Final step")
+# a=9
+
+# if a>=11:
+#     print("From If block")
+# elif a>=10:
+#     print("Elif Block 1")
+# elif a==10:
+#     print("Elif Block 2")
+# else:
+#     print("From else Block..!")
+
+# mark = int(input("Enter mark (out of 100): "))
+ 
+# if 90 <= mark <= 100:
+#     print("Grade: O")
+# elif 80 <= mark <= 89:
+#     print("Grade: A+")
+# elif 70 <= mark <= 79:
+#     print("Grade: A")
+# elif 60 <= mark <= 69:
+#     print("Grade: B")
+# elif 50 <= mark <= 59:
+#     print("Grade: C")
+# elif 0 <= mark <= 49:
+#     print("Grade: Fail")
+# else:
+#     print("Invalid mark! .")
+
+# mark = int(input("Enter your marks: "))
+ 
+# if mark >= 90 and mark <= 100:
+#     print("Grade: O")
+# elif mark >= 80 and mark <= 89:
+#     print("Grade: A+")
+# elif mark >= 70 and mark <= 79:
+#     print("Grade: A")
+# elif mark >= 60 and mark <= 69:  
+#     print("Grade: B")
+# elif mark >= 50 and mark <= 59:
+#     print("Grade: C")
+# elif mark >= 0 and mark <= 49:
+#     print("Grade: Fail")
+# else:
+#     print("Invalid Marks! Please enter marks between 0 and 100.")
+
+# a=10 
+
+# if a<15:
+#     print("Hi")
+# else:
+#     print("Hello")
+# if a>9:
+#     print("Hello world")
+# else:
+#     print("Hi World")
+
+# if True:
+#     if True:
+#         print("Hi")
+
+# a=int(input("Enter a number: "))
+
+# if a>10:
+#     if a>15:
+#         print("Very Large")
+#     else:
+#         print("Large")
+# else:
+#     if a<5:
+#         print("Very Small")
+#     else:
+#         print("Small")
