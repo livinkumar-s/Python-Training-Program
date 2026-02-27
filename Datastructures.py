bcc28273452dc425e9528ec48ab647a1dfb20f9a
LivinkumarStackly <livinkumar@thestackly.com>
2026-02-27 12:32:14 +0530

    Datastructures - List

---
+++ Datastructures.py
@@ -0,0 +1,38 @@
+# a="hello guys!!!!"
+# print(a[-2])
+
+# print(a[6:]) #"guys!!!!"
+# print(a[:6]) #"hello "
+# print(a[2:7]) 
+# print(a[::-1])
+
+# List 
+# lis1=[1,2,3,4,"Hello","Hi",True,False,22,33,33,[22,33,44]]
+# print(len(lis1))
+# print(lis1[5])
+# print(lis1[3:7]) #[]
+# print(lis1[0:7:2]) 
+
+# lis1.append(2)
+# lis1[3]+=2
+
+# print(lis1)
+
+# lis2.append([3,4,5])
+# lis2.append("Hello")
+# lis2.extend([5,6,7,8])
+
+# lis2.append(2)
+# lis2.extend(2)
+lis2=[213,234,234,234,3,5,234]
+# names=["Rohit","Virat","Dhoni","Rohit","Rohit"]
+
+# lis2.insert(4,"Hello")
+# lis2.remove(234)
+# lis2.pop(3)
+# lis2.pop()
+# lis2.clear()
+# lis2.sort()
+# lis2.reverse()
+# print(lis2)
+# print(lis2[::-1])
