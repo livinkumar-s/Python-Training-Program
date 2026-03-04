6e6ad0858976c33a5ca2a8f01af1d7bb89787619
LivinkumarStackly <livinkumar@thestackly.com>
2026-03-04 12:16:49 +0530

    Funtions

---
+++ Functions.py
@@ -0,0 +1,70 @@
+# def printSteps():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+
+
+# printSteps()
+
+# def printSum(a,b):
+#     # print(a+b)
+#     print("A:"+str(a))
+#     print("B:"+str(b))
+
+# printSum(10,12)
+# printSum(33,44)
+# printSum("Hello",5)
+
+# printSum(b=99,a=100)
+
+# def printMul(a,b=10):
+#     print(a*b)
+
+# printMul(4,4)
+# print(sum([123,213,231,213]))
+
+
+# def printSum(*a):
+#     total = 0
+#     for num in a:
+#         total += num
+#     print(total)
+ 
+# printSum(132,23,2)
+
+# def greet():
+#     print("Hi1")
+#     print("Hi2")
+#     print("Hi3")
+#     print("Hi4")
+#     print("Hi5")
+#     return 
+#     #it is returning None
+    
+
+# a=greet() #None
+# print(a)
+
+def printPro(a,b):
+    return a*b
+
+# print(printPro(9,printPro(4,printPro(5,printPro(3,3)))))
+
+# printPro()
+
+# print()
+# input()
+# int()
+
+# print(input()+" World")
+
+# lis1=[1,2,3,2,1]
+
+# print(lis1.reverse())
+# print(lis1[::-1])
+# print(lis1)
+
+# if lis1==lis1[::-1]:
+#     print("Pal")
+# else:
+#     print("No pal")
