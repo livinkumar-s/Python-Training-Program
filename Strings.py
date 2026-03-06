4bd99cb06c0cae4b9acc682214cf6bbf035b130c
LivinkumarStackly <livinkumar@thestackly.com>
2026-03-06 12:03:45 +0530

    String

---
+++ Strings.py
@@ -0,0 +1,65 @@
+# bigPara='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor.\nPellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.\nVivamus fermentum semper porta. Nunc diam velit, adipiscing ut tristique vitae, sagittis vel odio. Maecenas convallis ullamcorper ultricies. Curabitur ornare, ligula semper consectetur sagittis, nisi diam iaculis velit, id fringilla sem nunc vel mi. Nam dictum, odio nec pretium volutpat, arcu ante placerat erat, non tristique elit urna et turpis. Quisque mi metus, ornare sit amet fermentum et, tincidunt et orci. Fusce eget orci a orci congue vestibulum. Ut dolor diam, elementum et vestibulum eu, porttitor vel elit. Curabitur venenatis pulvinar tellus gravida ornare. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+# bigPara='''Hello
+# Hi
+# Welcome'''
+
+# print(bigPara)
+
+# lis=[1,2,3,4,5]
+# lis.append(6)
+# print(lis)
+
+# str1="Hello"
+# # str1[1]="z"
+# str1+=" World" # new string is created
+# # print(str1)
+
+# print(str1*6)
+
+# str1="hello world hello guys hello boys"
+# print(str1.upper())
+# print(str1)
+# print(str1.lower())
+# print(str1.title())
+# print(str1.capitalize())
+# print(str1.strip())
+# print(str1.rstrip()+"!")
+# print(str1.lstrip()+"!")
+# print(str1.replace('hello','hi'))
+
+# lis1=[1,2,3,4]
+# lis1.count()
+
+# str1="Hello guys. I am From Tamilnadu!" # multiple strs
+#["Hello guys. I ","m From T","miln","du"]
+# print(str1.split())
+
+# print("---".join(("Hi","Hello","Welcome")))
+
+# name="Vikram"
+# age=58
+
+# print("His name is "+name+". He is "+str(age)+" years old!")
+# print(f'His name is {name}. He is {age} years old!')
+
+# MEmbership
+
+# print("" in "Hello world")
+# print("h" not in "Hello world")
+
+# print(1 is 1)
+# a=1 
+# b=1 
+# print(a is b)
+# print(a==b)
+
+# lis1=[1,2,3]
+# lis2=[1,2,3]
+# print(lis1==lis2)
+# print(lis1 is not lis2)
+
+# UserDetails=None 
+# UserId=None
+# UserDetails={}
+# UserDetails is None
